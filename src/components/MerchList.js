10696cd1edd618be61d5282ee33c8479e134a4b2
Helen Lehrer <hlehrer@outlook.com>
2022-11-16 09:37:14 -0800

    Add MerchList component

--- src/components/MerchList.js
+++ src/components/MerchList.js
@@ -3,5 +3,20 @@ import Merch from "./Merch";
 import PropTypes from "prop-types";
 
 function MerchList(props){
-  
+return (
+  <React.Fragment>
+    {props.merchList.map((merch, index) =>
+    <Merch name ={merch.name}
+    description ={merch.description}
+    quantity ={merch.quantity} 
+    key={index} />
+    )}
+  </React.Fragment>
+  );
 }
+
+MerchList.propTypes = {
+  merchList: PropTypes.array
+};
+
+export default MerchList;
